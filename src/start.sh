2f9516b5dc7dfa59a22ff6e2facf27ed6b00295b
Vatsal Shah <vatsalshahxa@gmail.com>
2024-06-18 22:40:11 +0530

    fix: allow additional flags to accept arguments

--- src/start.sh
+++ src/start.sh
@@ -8,12 +8,18 @@ usage() {
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --hf_token) HF_TOKEN="$2"; shift ;;
-        --flock_api_key) FLOCK_API_KEY="$2"; shift ;;
-        --task_id) TASK_ID="$2"; shift ;;
-        *) OTHER_ARGS+="$1 "; shift ;;  # Collect other arguments
+        --hf_token) HF_TOKEN="$2"; shift 2 ;;
+        --flock_api_key) FLOCK_API_KEY="$2"; shift 2 ;;
+        --task_id) TASK_ID="$2"; shift 2 ;;
+        --validation_args_file) VALIDATION_ARGS_FILE="$2"; shift 2 ;;
+        *) 
+            if [[ "$2" != --* ]]; then
+                OTHER_ARGS+="$1 $2 "; shift 2
+            else
+                OTHER_ARGS+="$1 "; shift
+            fi
+            ;;
     esac
-    shift
 done
 
 if [ -z "$HF_TOKEN" ] || [ -z "$FLOCK_API_KEY" ] || [ -z "$TASK_ID" ]; then
@@ -24,7 +30,7 @@ export HF_TOKEN
 export FLOCK_API_KEY
 
 while true; do
-    python validate.py loop --task_id ${TASK_ID} ${OTHER_ARGS}
+    python validate.py loop --task_id ${TASK_ID} --validation_args_file ${VALIDATION_ARGS_FILE} ${OTHER_ARGS}
     EXIT_CODE=$?
     if [ $EXIT_CODE -eq 100 ]; then
         echo "CUDA error detected, restarting the process..."
