13b640278eb9c225236f63ba01e32a18aa978303
Nick W <52875892+nickcom007@users.noreply.github.com>
2025-08-04 12:31:40 -0400

    Update constant.py

--- src/core/constant.py
+++ src/core/constant.py
@@ -1,28 +1,4 @@
 SUPPORTED_BASE_MODELS = [
-    # qwen1.5
-    "Qwen/Qwen1.5-0.5B",
-    "Qwen/Qwen1.5-0.5B-Chat",
-    "Qwen/Qwen1.5-1.8B",
-    "Qwen/Qwen1.5-1.8B-Chat",
-    "Qwen/Qwen1.5-4B",
-    "Qwen/Qwen1.5-4B-Chat",
-    "Qwen/Qwen1.5-7B",
-    "Qwen/Qwen1.5-7B-Chat",
-    "Qwen/Qwen1.5-14B",
-    "Qwen/Qwen1.5-14B-Chat",
-    "Qwen/Qwen1.5-32B",
-    "Qwen/Qwen1.5-32B-Chat",
-    "Qwen/Qwen1.5-72B",
-    "Qwen/Qwen1.5-72B-Chat",
-    # qwen2
-    "Qwen/Qwen2-0.5B",
-    "Qwen/Qwen2-0.5B-Instruct",
-    "Qwen/Qwen2-1.5B",
-    "Qwen/Qwen2-1.5B-Instruct",
-    "Qwen/Qwen2-7B",
-    "Qwen/Qwen2-7B-Instruct",
-    "Qwen/Qwen2-72B",
-    "Qwen/Qwen2-72B-Instruct",
     # qwen2.5
     "Qwen/Qwen2.5-0.5B",
     "Qwen/Qwen2.5-0.5B-Instruct",
@@ -38,13 +14,6 @@ SUPPORTED_BASE_MODELS = [
     "Qwen/Qwen2.5-32B-Instruct",
     "Qwen/Qwen2.5-72B",
     "Qwen/Qwen2.5-72B-Instruct",
-    # Yi
-    "01-ai/Yi-6B",
-    "01-ai/Yi-6B-Chat",
-    "01-ai/Yi-9B",
-    "01-ai/Yi-9B-Chat",
-    "01-ai/Yi-34B",
-    "01-ai/Yi-34B-Chat",
     # yi 1.5
     "01-ai/Yi-1.5-6B",
     "01-ai/Yi-1.5-6B-Chat",
@@ -53,20 +22,9 @@ SUPPORTED_BASE_MODELS = [
     "01-ai/Yi-1.5-34B",
     "01-ai/Yi-1.5-34B-Chat",
     # mistral
-    "mistralai/Mistral-7B-v0.1",
     "mistralai/Mistral-7B-v0.3",
-    "mistralai/Mistral-7B-Instruct-v0.1",
-    "mistralai/Mistral-7B-Instruct-v0.2",
     "mistralai/Mistral-7B-Instruct-v0.3",
     "mistralai/Ministral-8B-Instruct-2410",
-    # mixtral
-    "mistralai/Mixtral-8x7B-v0.1",
-    "mistralai/Mixtral-8x7B-Instruct-v0.1",
-    # gemma
-    "google/gemma-2b",
-    "google/gemma-7b",
-    "google/gemma-2b-it",
-    "google/gemma-7b-it",
     # gemma2
     "google/gemma-2-2b",
     "google/gemma-2-9b",
@@ -74,16 +32,6 @@ SUPPORTED_BASE_MODELS = [
     "google/gemma-2-2b-it",
     "google/gemma-2-9b-it",
     "google/gemma-2-27b-it",
-    # zephyr
-    "HuggingFaceH4/zephyr-7b-alpha",
-    "HuggingFaceH4/zephyr-7b-beta",
-    # llama2
-    "meta-llama/Llama-2-7b-hf",
-    "meta-llama/Llama-2-13b-hf",
-    "meta-llama/Llama-2-70b-hf",
-    "meta-llama/Llama-2-7b-chat-hf",
-    "meta-llama/Llama-2-13b-chat-hf",
-    "meta-llama/Llama-2-70b-chat-hf",
     # llama3
     "meta-llama/Meta-Llama-3-8B",
     "meta-llama/Meta-Llama-3-8B-Instruct",
@@ -101,11 +49,4 @@ SUPPORTED_BASE_MODELS = [
     # phi4
     "microsoft/Phi-4-mini-instruct",
     "microsoft/phi-4",
-    # deepseek
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B",
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-70B",
 ]
